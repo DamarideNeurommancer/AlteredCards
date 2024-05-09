ff158d61222167b5e2c680b245cb7d12a7316551
DamarideNeurommancer <134946304+DamarideNeurommancer@users.noreply.github.com>
2024-05-09 17:50:41 +0200

    Add files via upload

--- Misc.js
+++ Misc.js
@@ -1283,7 +1283,7 @@ const msg="Score drawn by \u00A9 Vex Flow";
  }
  else{
   tempCanvas.fillText(name+" "+wmt,40,18);
-  tempCanvas.font="8px verdana";
+  //tempCanvas.font="8px verdana";
   tempCanvas.fillText(msg,40,ch-30);
   tempCanvas.fillText(msg,cw-300,18);
  }
